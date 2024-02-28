c3e43e2f8fc95b87e1c949fae76f57edbbf67345
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-02-28 22:34:12 +0700

    #003 Add Snake Case Config

---
+++ BirthdayBlitzAPI/Common/SnakecasingParameOperationFilter.cs
@@ -0,0 +1,26 @@
+﻿using BusinessObjects.Common.Extensions;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayBlitzAPI.Common
+{
+    public class SnakecasingParameOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null) operation.Parameters = new List<OpenApiParameter>();
+            else
+            {
+                foreach (var item in operation.Parameters)
+                {
+                    item.Name = item.Name.ToSnakeCase();
+                }
+            }
+        }
+    }
+}
