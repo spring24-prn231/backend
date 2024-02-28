b914d2cb08cd658c8c70af0f6e6beea52a1c567e
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-02-28 22:34:29 +0700

    #003 Add Extensions

---
+++ BusinessObjects/Common/Extensions/QueryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AutoFilterer.Extensions;
+using BusinessObjects.Common.Exceptions;
+using BusinessObjects.Models;
+using BusinessObjects.Responses;
+
+namespace BusinessObjects.Common.Extensions
+{
+    public static class QueryExtensions
+    {
+        public static AppResponse<IEnumerable<T>>? GetPaginatedResponse<T>(this IQueryable<T> source, int page = 1, int pageSize = 10)
+        {
+            var itemCount = source.Count();
+            if (itemCount == 0) throw new NotFoundException();
+            return new PaginationResponse<IEnumerable<T>>
+            {
+                PageSize = pageSize,
+                TotalPage = (int)Math.Ceiling((decimal)itemCount / pageSize),
+                CurrentPage = page,
+                Data = source.ToPaged(page, pageSize).ToList()
+            };
+        }
+        public static IQueryable<T> GetQueryStatusTrue<T>(this IQueryable<T> source) where T : BaseModel
+        {
+            return source.Where(x => x.Status == true);
+        }
+    }
+}
---
+++ BusinessObjects/Common/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string o) =>
+               Regex.Replace(o, @"(\w)([A-Z])", "$1_$2").ToLower();
+    }
+}
