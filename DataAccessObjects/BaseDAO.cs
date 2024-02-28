56fc6e243257e27b4fd7fe477abb966fef3e34df
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-02-28 22:38:59 +0700

    #003 Build Base Layers

---
+++ DataAccessObjects/BaseDAO.cs
@@ -0,0 +1,95 @@
+﻿using BusinessObjects.Models;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Common.Extensions;
+
+namespace DataAccessObjects
+{
+    public class BaseDAO<T> : IBaseDAO<T> where T : BaseModel
+    {
+        protected readonly BirthdayBlitzContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public BaseDAO(BirthdayBlitzContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dbSet.AsQueryable();
+        }
+        public virtual void Create(T entity)
+        {
+            for (int i=0;i<=2;i++)
+            {
+                try
+                {
+                    _dbSet.Add(entity);
+                    _context.SaveChanges();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    _context.ChangeTracker.Clear();
+                    if(i == 2)
+                    {
+                        throw;
+                    }
+                    continue;
+                }
+            }
+        }
+        public virtual void Update(T entity)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                try
+                {
+                    var tracker = _context.Attach(entity);
+                    tracker.State = EntityState.Modified;
+                    _context.SaveChanges();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    _context.ChangeTracker.Clear();
+                    if (i == 2)
+                    {
+                        throw;
+                    }
+                    continue;
+                }
+            }
+        }
+        public virtual void Delete(T entity)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                try
+                {
+                    _dbSet.Remove(entity);
+                    _context.SaveChanges();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    _context.ChangeTracker.Clear();
+                    if (i == 2)
+                    {
+                        throw;
+                    }
+                    continue;
+                }
+            }
+        }
+
+        public T? GetById(Guid id)
+        {
+            return _dbSet.FirstOrDefault(x => x.Id == id);
+        }
+
+        public T? GetByIdNoTracking(Guid id)
+        {
+            return _dbSet.AsNoTracking().FirstOrDefault(x=>x.Id == id);
+        }
+    }
+}
---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using BusinessObjects.Models;
+using DataAccessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseModel
+    {
+        protected readonly IBaseDAO<T> _dao;
+        public BaseRepository(IBaseDAO<T> dao)
+        {
+            _dao = dao;
+        }
+        public virtual void Create(T entity)
+        {
+            _dao.Create(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            entity.Status = false;
+            _dao.Update(entity);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dao.GetAll();
+        }
+
+        public virtual T? GetById(Guid id)
+        {
+            return _dao.GetById(id);
+        }
+
+        public virtual T? GetByIdNoTracking(Guid id)
+        {
+            return _dao.GetByIdNoTracking(id);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _dao.Update(entity);
+        }
+    }
+}
