7ab7143e8f6ba1b801c53dc8a77ddaaff417666d
minhncn2310 <minh27122002pro@gmail.com>
2024-02-29 12:06:53 +0700

    #9 CRUD Menu

---
+++ BusinessObjects/Requests/CreateMenuRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Requests
+{
+    public class CreateMenuRequest
+    {
+        public Guid? DishId { get; set; }
+
+        public Guid? ServiceId { get; set; }
+    }
+}
---
+++ BusinessObjects/Requests/UpdateMenuRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Requests
+{
+    public class UpdateMenuRequest : BaseUpdateRequest
+    {
+        public Guid? DishId { get; set; }
+
+        public Guid? ServiceId { get; set; }
+    }
+}
