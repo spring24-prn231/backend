04691f66cb5f7116736747ea82ccfd454845e3eb
minhncn2310 <minh27122002pro@gmail.com>
2024-03-01 23:49:04 +0700

    #14 CRUD RoomType

---
+++ BirthdayBlitzAPI/Controllers/RoomTypeController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessObjects.Common.Enums;
+using BusinessObjects.Common.Extensions;
+using BusinessObjects.Requests;
+using BusinessObjects.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace BirthdayBlitzAPI.Controllers
+{
+    public class RoomTypeController : ApiControllerBase
+    {
+        public IRoomTypeService _roomTypeService;
+        public RoomTypeController(IRoomTypeService roomTypeService)
+        {
+            _roomTypeService = roomTypeService;
+        }
+        [HttpGet]
+        public IActionResult Get([FromQuery] GetRoomTypeFilterRequest filter)
+        {
+            var response = _roomTypeService.Get(filter).GetPaginatedResponse(page: filter.Page, pageSize: filter.PageSize);
+            return Ok(response);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateRoomTypeRequest request)
+        {
+            _roomTypeService.Create(request);
+            return Ok(new AppResponse<object>
+            {
+                Message = MessageResponse.CreateSuccess
+            });
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] UpdateRoomTypeRequest request)
+        {
+            _roomTypeService.Update(request);
+            return Ok(new AppResponse<object>
+            {
+                Message = MessageResponse.UpdateSuccess
+            });
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            _roomTypeService.Delete(id);
+            return Ok(new AppResponse<object>
+            {
+                Message = MessageResponse.DeleteSuccess
+            });
+        }
+    }
+}
---
+++ BusinessObjects/Requests/GetRoomTypeFilterRequest.cs
@@ -0,0 +1,18 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Requests
+{
+    public class GetRoomTypeFilterRequest : BasePaginationRequest
+    {
+        [ToLowerContainsComparison]
+        [StringFilterOptions(StringFilterOption.Contains, StringComparison.InvariantCultureIgnoreCase)]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
