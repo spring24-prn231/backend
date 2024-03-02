9daaa3d6290641f9cabac92b7a352134af5a7b06
minhncn2310 <minh27122002pro@gmail.com>
2024-03-02 11:27:16 +0700

    #18 CRUD Slot, Add validation for check valid hour and slot is available or not

---
+++ BirthdayBlitzAPI/MappingProfiles/SlotProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BusinessObjects.Models;
+using BusinessObjects.Requests;
+
+namespace BirthdayBlitzAPI.MappingProfiles
+{
+    public class SlotProfile : Profile
+    {
+        public SlotProfile()
+        {
+            CreateMap<CreateSlotRequest, Slot>()
+                .ForMember(dest => dest.Room, opt => opt.Ignore());
+            CreateMap<UpdateSlotRequest, Slot>()
+                .ForMember(x => x.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Room, opt => opt.Ignore())
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+        }
+    }
+}
