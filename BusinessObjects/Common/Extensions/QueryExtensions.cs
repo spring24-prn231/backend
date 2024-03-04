9758d86e142d171c34d9707a78e7677056561ee8
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-03-05 01:28:22 +0700

    #025 - Change Pagination Library

--- BusinessObjects/Common/Extensions/QueryExtensions.cs
+++ BusinessObjects/Common/Extensions/QueryExtensions.cs
@@ -2,7 +2,7 @@
 using BusinessObjects.Common.Exceptions;
 using BusinessObjects.Models;
 using BusinessObjects.Responses;
-using PagedList;
+using X.PagedList;
 
 namespace BusinessObjects.Common.Extensions
 {
@@ -10,15 +10,14 @@ namespace BusinessObjects.Common.Extensions
     {
         public static AppResponse<IEnumerable<T>>? GetPaginatedResponse<T>(this IQueryable<T> source, int page = 1, int pageSize = 10)
         {
-            var queryResult = source.ToList();
-            var itemCount = queryResult.Count();
-            if (itemCount == 0) throw new NotFoundException();
+            var paginatedResult = source.ToPagedList(page, pageSize);
+            if (paginatedResult.TotalItemCount == 0) throw new NotFoundException();
             return new PaginationResponse<IEnumerable<T>>
             {
                 PageSize = pageSize,
-                TotalPage = (int)Math.Ceiling((decimal)itemCount / pageSize),
+                TotalPage = paginatedResult.PageCount,
                 CurrentPage = page,
-                Data = queryResult.ToPagedList(page,pageSize)
+                Data = paginatedResult
             };
         }
         public static IQueryable<T> GetQueryStatusTrue<T>(this IQueryable<T> source) where T : BaseModel
