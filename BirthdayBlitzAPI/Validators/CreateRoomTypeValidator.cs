7a4f3698936328fe24bbb9092f6e012fd0c6311c
minhncn2310 <minh27122002pro@gmail.com>
2024-03-05 23:26:36 +0700

    #14 - Validation for RoomType

---
+++ BirthdayBlitzAPI/Validators/CreateRoomTypeValidator.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjects.Requests;
+using FluentValidation;
+using Services.Interfaces;
+
+namespace BirthdayBlitzAPI.Validators
+{
+    public class CreateRoomTypeValidator : AbstractValidator<CreateRoomTypeRequest>
+    {
+        private readonly IRoomTypeService _roomTypeService;
+        public CreateRoomTypeValidator(IRoomTypeService roomTypeService)
+        {
+            _roomTypeService = roomTypeService;
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Không được để trống loại phòng")
+                .MaximumLength(100).WithMessage("Phân loại phòng không quá 100 kí tự");
+            RuleFor(x => x.Description)
+                .MaximumLength(255).WithMessage("Mô tả không quá 255 kí tự");
+        }
+    }
+}
