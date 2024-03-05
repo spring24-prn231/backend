ce0f152059d960ef656e93d9577f31416f44d250
hoangdapoet <huynhnhatthienhoang2002@gmail.com>
2024-03-05 22:19:05 +0700

    #17 CRUD ServiceElementDetail

---
+++ BirthdayBlitzAPI/Validators/CreateServiceElementDetailValidator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessObjects.Requests;
+using FluentValidation;
+using Services.Interfaces;
+
+namespace BirthdayBlitzAPI.Validators
+{
+    public class CreateServiceElementDetailValidator : AbstractValidator<CreateServiceElementDetailRequest>
+    {
+        private readonly IServiceService _serviceService;
+        private readonly IServiceElementService _serviceElementService;
+        public CreateServiceElementDetailValidator(IServiceService serviceService, IServiceElementService serviceElementService)
+        {
+            _serviceService = serviceService;
+            _serviceElementService = serviceElementService;
+            RuleFor(x => x.ServiceId).Must(x => _serviceService.GetByIdNoTracking(x) != null)
+                .WithMessage("Service không tồn tại");
+            RuleFor(x => x.ServiceElementId).Must(x => _serviceElementService.GetByIdNoTracking(x) != null)
+                .WithMessage("ServiceElement không tồn tại");
+        }
+    }
+}
