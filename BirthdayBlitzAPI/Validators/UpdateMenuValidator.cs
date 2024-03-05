8f1b5091f0940807d76127256a7b54a16059060c
minhncn2310 <minh27122002pro@gmail.com>
2024-03-05 22:11:53 +0700

    #9 - Add validation for Menu

---
+++ BirthdayBlitzAPI/Validators/UpdateMenuValidator.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjects.Requests;
+using FluentValidation;
+using Services.Interfaces;
+
+namespace BirthdayBlitzAPI.Validators
+{
+    public class UpdateMenuValidator : AbstractValidator<UpdateMenuRequest>
+    {
+        private readonly IMenuService _menuService;
+        public UpdateMenuValidator(IMenuService menuService)
+        {
+            _menuService = menuService;
+            RuleFor(x => x.DishId)
+                .Must(x => _menuService.GetByIdNoTracking(x) != null)
+                .WithMessage("Dish không tồn tại");
+            RuleFor(x => x.ServiceId)
+                .Must(x => _menuService.GetByIdNoTracking(x) != null)
+                .WithMessage("Service không tồn tại");
+        }
+    }
+}
