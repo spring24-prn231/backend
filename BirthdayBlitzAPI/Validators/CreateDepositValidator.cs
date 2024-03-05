49d44e4affb6b0a39afd5d1bf75a70b87418cd94
hoangdapoet <huynhnhatthienhoang2002@gmail.com>
2024-03-05 21:43:23 +0700

    #5 modify validation create/update deposit

--- BirthdayBlitzAPI/Validators/CreateDepositValidator.cs
+++ BirthdayBlitzAPI/Validators/CreateDepositValidator.cs
@@ -15,8 +15,10 @@ namespace BirthdayBlitzAPI.Validators
         {
             _orderService = orderService;
             RuleFor(x => x.Value).GreaterThanOrEqualTo(0)
-                .WithMessage("Tiền đặt cọc phải lớn hơn 0đ");
-            RuleFor(x => x.OrderId).Must(x => _orderService.        GetByIdNoTracking(x) != null)
+                .WithMessage("Tiền đặt cọc phải lớn hơn 0đ")
+                .PrecisionScale(1, 20, false)
+                .WithMessage("Tiền đặt cọc không hợp lệ");
+            RuleFor(x => x.OrderId).Must(x => _orderService.GetByIdNoTracking(x) != null)
                 .WithMessage("Order không tồn tại");
         }
     }
