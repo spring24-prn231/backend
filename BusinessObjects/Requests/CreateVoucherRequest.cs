971195f140483ca67727cb4c9ebe190e03ffc4c4
hoangdapoet <huynhnhatthienhoang2002@gmail.com>
2024-03-05 09:53:57 +0700

    #1 CRUD Voucher

---
+++ BusinessObjects/Requests/CreateVoucherRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Requests
+{
+    public class CreateVoucherRequest
+    {
+        public Guid OrderId { get; set; }
+        [MaxLength(20)]
+        public string? Code { get; set; }
+        [Range(0, int.MaxValue)]
+        public int? Discount { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal? MaximumValue { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+    }
+}
---
+++ BusinessObjects/Requests/GetVoucherFilterRequest.cs
@@ -0,0 +1,24 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Types;
+using AutoFilterer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessObjects.Requests
+{
+    public class GetVoucherFilterRequest : BasePaginationRequest
+    {
+        public Guid? Id { get; set; }
+        public Guid? OrderId { get; set; }
+        [ToLowerContainsComparison]
+        [StringFilterOptions(StringFilterOption.Contains, StringComparison.InvariantCultureIgnoreCase)]
+        public string? Code { get; set; }
+        public Range<int>? Discount { get; set; }
+        public Range<decimal>? MaximumValue { get; set; }
+        public Range<DateTime>? ExpirationDate { get; set; }
+    }
+}
