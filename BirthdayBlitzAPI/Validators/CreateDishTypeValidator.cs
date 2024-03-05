c345da2ceea299c0968e6bd8f4213dccfa055166
minhncn2310 <minh27122002pro@gmail.com>
2024-03-05 22:02:00 +0700

    #7 - Add validation for Dish Type

---
+++ BirthdayBlitzAPI/Validators/CreateDishTypeValidator.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects.Requests;
+using FluentValidation;
+using Services.Interfaces;
+
+namespace BirthdayBlitzAPI.Validators
+{
+    public class CreateDishTypeValidator : AbstractValidator<CreateDishTypeRequest>
+    {
+        private readonly IDishTypeService _dishTypeService;
+        public CreateDishTypeValidator(IDishTypeService dishTypeService)
+        {
+            _dishTypeService = dishTypeService;
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Tên không được để trống")
+                .MaximumLength(100).WithMessage("Loại món ăn không quá 100 kí tự");
+        }
+    }
+}
---
+++ BirthdayBlitzAPI/Validators/UpdateDishTypeValidator.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects.Requests;
+using FluentValidation;
+using Services.Interfaces;
+
+namespace BirthdayBlitzAPI.Validators
+{
+    public class UpdateDishTypeValidator : AbstractValidator<UpdateDishTypeRequest>
+    {
+        private readonly IDishTypeService _dishTypeService;
+        public UpdateDishTypeValidator(IDishTypeService dishTypeService)
+        {
+            _dishTypeService = dishTypeService;
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Tên không được để trống")
+                .MaximumLength(100).WithMessage("Loại món ăn không quá 100 kí tự");
+        }
+    }
+}
