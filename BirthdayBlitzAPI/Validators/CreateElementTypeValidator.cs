e5bce77dd3104cff79ee4fe78a7ea3b3f647eea7
hoangdapoet <huynhnhatthienhoang2002@gmail.com>
2024-03-05 11:01:53 +0700

    #8 CRUD ElementType

---
+++ BirthdayBlitzAPI/Validators/CreateElementTypeValidator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessObjects.Requests;
+using FluentValidation;
+
+namespace BirthdayBlitzAPI.Validators
+{
+    public class CreateElementTypeValidator : AbstractValidator<CreateElementTypeRequest>
+    {
+        public CreateElementTypeValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Tên không được để trống!")
+                .MaximumLength(100)
+                .WithMessage("Tên không được quá 100 ký tự!");
+        }
+    }
+}
