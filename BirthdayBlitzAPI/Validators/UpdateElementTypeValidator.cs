84b9ed31042e0f4121d081dd38ef73cb39b88f1d
hoangdapoet <huynhnhatthienhoang2002@gmail.com>
2024-03-05 21:46:28 +0700

    #8 modify validation create/update ElementType

--- BirthdayBlitzAPI/Validators/UpdateElementTypeValidator.cs
+++ BirthdayBlitzAPI/Validators/UpdateElementTypeValidator.cs
@@ -12,8 +12,6 @@ namespace BirthdayBlitzAPI.Validators
         public UpdateElementTypeValidator()
         {
             RuleFor(x => x.Name)
-                .NotEmpty()
-                .WithMessage("Tên không được để trống!")
                 .MaximumLength(100)
                 .WithMessage("Tên không được quá 100 ký tự!");
         }
--- BusinessObjects/Requests/UpdateElementTypeRequest.cs
+++ BusinessObjects/Requests/UpdateElementTypeRequest.cs
@@ -7,6 +7,6 @@ namespace BusinessObjects.Requests
 {
     public class UpdateElementTypeRequest : BaseUpdateRequest
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
     }
 }
