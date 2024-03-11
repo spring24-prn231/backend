aec740dd59efae026f7af846ae66915aad49e8a5
hoangdapoet <huynhnhatthienhoang2002@gmail.com>
2024-03-11 15:13:05 +0700

    #15 moidfy CreateServiceValidator

--- BirthdayBlitzAPI/Validators/CreateServiceValidator.cs
+++ BirthdayBlitzAPI/Validators/CreateServiceValidator.cs
@@ -27,10 +27,18 @@ namespace BirthdayBlitzAPI.Validators
                 .WithMessage("Mô tả dịch vụ không được quá 255 ký tự");
             RuleFor(x => x.RoomTypeId).Must(x => _roomTypeService.GetByIdNoTracking(x) != null)
                 .WithMessage("Loại phòng không tồn tại");
-            RuleFor(x => x.ServiceElementIds).Must(x => x.All(y => _serviceElementDetailService.GetByIdNoTracking(y) != null))
-                .WithMessage("Dịch vụ không tồn tại");
-            RuleFor(x => x.DishIds).Must(x => x.All(y => _menuService.GetByIdNoTracking(y) != null))
-                .WithMessage("Món ăn không tồn tại trong Menu");
+            RuleFor(x => x.ServiceElementIds)
+                .ChildRules(child => {
+                    child.RuleFor(x => x)
+                        .Must(x => x.All(y => _serviceElementDetailService.GetByIdNoTracking(y) != null))
+                        .WithMessage("ServiceElement không tồn tại");
+                });
+            RuleFor(x => x.DishIds)
+                .ChildRules(child => {
+                    child.RuleFor(x => x)
+                        .Must(x => x.All(y => _menuService.GetByIdNoTracking(y) != null))
+                        .WithMessage("Món ăn không tồn tại trong Menu");
+                });
         }
     }
 }
