38dcd7b07413138125ee34f71446698971762729
minhncn2310 <minh27122002pro@gmail.com>
2024-03-11 11:12:42 +0700

    #6 - Resolve Issue

---
+++ BusinessObjects/Requests/GetDishFilterRequest.cs
@@ -0,0 +1,21 @@
+﻿using AutoFilterer.Attributes;
+using AutoFilterer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Requests
+{
+    public class GetDishFilterRequest : BasePaginationRequest
+    {
+        [ToLowerContainsComparison]
+        [StringFilterOptions(StringFilterOption.Contains, StringComparison.InvariantCultureIgnoreCase)]
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        [ToLowerContainsComparison]
+        [StringFilterOptions(StringFilterOption.Contains, StringComparison.InvariantCultureIgnoreCase)]
+        public string? Description { get; set; }
+    }
+}
