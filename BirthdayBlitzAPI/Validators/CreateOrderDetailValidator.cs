bbd862f41ff3e4a9d79ff46c00d16b6ef16a4ed1
minhncn2310 <minh27122002pro@gmail.com>
2024-03-16 00:35:17 +0700

    #11 - Manage Order Detail

---
+++ BirthdayBlitzAPI/Validators/CreateOrderDetailValidator.cs
@@ -0,0 +1,35 @@
+﻿using BusinessObjects.Requests;
+using FluentValidation;
+using Services.Implements;
+using Services.Interfaces;
+
+namespace BirthdayBlitzAPI.Validators
+{
+    public class CreateOrderDetailValidator : AbstractValidator<CreateOrderDetailRequest>
+    {
+        private readonly IOrderService _orderService;
+
+        public CreateOrderDetailValidator(IOrderService orderService)
+        {
+            _orderService = orderService;
+            RuleFor(x => x.OrderId)
+               .MustAsync(async (x, cancellationToken) => await _orderService.GetByIdNoTracking(x) != null)
+               .WithMessage("Id của đơn hàng này không tồn tại");
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("Giá tiền phải lớn hơn 0");
+            RuleFor(x => x.Cost)
+                .GreaterThan(0)
+                .WithMessage("Tiền phải lớn hơn 0");
+            RuleFor(x => x.Note)
+                .Length(255)
+                .WithMessage("Ghi chú về chi tiết đơn hàng không vượt quá 255 kí tự");
+            RuleFor(x => x.Amount)
+                .GreaterThan(0)
+                .WithMessage("Số lượng trong chi tiết đơn hàng phải lớn hơn 0");
+            RuleFor(x => x.Type)
+                .Length(255)
+                .WithMessage("Loại của chi tiết đơn hàng không vượt quá 255 kí tự");
+        }
+    }
+}
---
+++ BusinessObjects/Requests/CreateOrderDetailRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Requests
+{
+    public class CreateOrderDetailRequest
+    {
+        public Guid OrderId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Type { get; set; }
+
+        public decimal Cost { get; set; }
+        public string Note { get; set; }
+    }
+}
