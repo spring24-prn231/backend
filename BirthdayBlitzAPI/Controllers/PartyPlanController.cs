b5c1c2953121bb1cd3a68e1280a241b594671549
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-03-16 21:38:51 +0700

    #060 AuthorizePartyPlan, Add Approve API

--- BirthdayBlitzAPI/Controllers/PartyPlanController.cs
+++ BirthdayBlitzAPI/Controllers/PartyPlanController.cs
@@ -2,6 +2,7 @@
 using BusinessObjects.Common.Extensions;
 using BusinessObjects.Requests;
 using BusinessObjects.Responses;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 
@@ -10,16 +11,20 @@ namespace BirthdayBlitzAPI.Controllers
     public class PartyPlanController : ApiControllerBase
     {
         private readonly IPartyPlanService _partyPlanService;
-        public PartyPlanController(IPartyPlanService partyPlanService) 
+        private readonly IOrderService _orderService;
+        public PartyPlanController(IPartyPlanService partyPlanService, IOrderService orderService) 
         {
+            _orderService = orderService;
             _partyPlanService = partyPlanService;
         }
+        [Authorize(Roles = "ADMIN, HOST_STAFF")]
         [HttpGet]
         public async Task<IActionResult> Get([FromQuery] GetPartyPlanFilterRequest filter)
         {
             var response = await _partyPlanService.Get(filter).GetPaginatedResponse(page: filter.Page, pageSize: filter.PageSize);
             return Ok(response);
         }
+        [Authorize(Roles = "HOST_STAFF")]
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreatePartyPlanRequest request)
         {
@@ -29,6 +34,7 @@ namespace BirthdayBlitzAPI.Controllers
                 Message = MessageResponse.CreateSuccess
             });
         }
+        [Authorize(Roles = "HOST_STAFF")]
         [HttpPost("list")]
         public async Task<IActionResult> CreatePartyPlanByList([FromBody] List<CreatePartyPlanRequest> requests)
         {
@@ -42,6 +48,7 @@ namespace BirthdayBlitzAPI.Controllers
                 Message = MessageResponse.CreateSuccess
             });
         }
+        [Authorize(Roles = "HOST_STAFF, ADMIN")]
         [HttpPut]
         public async Task<IActionResult> Update([FromBody] UpdatePartyPlanRequest request)
         {
@@ -51,6 +58,7 @@ namespace BirthdayBlitzAPI.Controllers
                 Message = MessageResponse.UpdateSuccess
             });
         }
+        [Authorize(Roles = "HOST_STAFF, ADMIN")]
         [HttpPut("list")]
         public async Task<IActionResult> UpdatePartyPlan([FromBody] List<UpdatePartyPlanRequest> requests)
         {
@@ -64,13 +72,24 @@ namespace BirthdayBlitzAPI.Controllers
                 Message = MessageResponse.UpdateSuccess
             });
         }
-        [HttpDelete]
+        [Authorize(Roles = "HOST_STAFF")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(Guid id)
         {
             await _partyPlanService.Delete(id);
             return Ok(new AppResponse<object>
                 { 
                 Message = MessageResponse.DeleteSuccess 
+            });
+        }
+        [Authorize(Roles = "ADMIN")]
+        [HttpPost("approvement")]
+        public async Task<IActionResult> ApprovePlan([FromBody] ApprovePlanRequest request)
+        {
+            await _orderService.Approve(request);
+            return Ok(new AppResponse<object>
+            {
+                Message = MessageResponse.UpdateSuccess
             });
         }
     }
